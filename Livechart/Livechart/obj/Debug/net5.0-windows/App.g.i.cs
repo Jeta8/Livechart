793409cffd90867a2beaa33991f4cac3dad87c3b
Jederson Gabriel <89799559+Jeta8@users.noreply.github.com>
2022-10-18 12:08:23 -0300

    update 18/10

--- Livechart/Livechart/obj/Debug/net5.0-windows/App.g.i.cs
+++ Livechart/Livechart/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Livechart {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Livechart {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public static void Main() {
             Livechart.App app = new Livechart.App();
             app.InitializeComponent();
